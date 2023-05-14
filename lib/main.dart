7fa321c28047ebedbf281ca296408d3aad4e1346
Kate Genegaban <adriankate.genegaban@wvsu.edu.ph>
2023-05-14 22:00:53 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_layout/basic_screen.dart';
 import 'package:flutter_layout/home.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() => runApp(StaticApp());
 
