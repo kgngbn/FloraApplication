03c4e9f7ceed160d7fa69adc81b2a2f5de0bb1ca
Kate Genegaban <adriankate.genegaban@wvsu.edu.ph>
2023-05-22 07:51:17 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_layout/home.dart';
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(StaticApp());
 }
 
